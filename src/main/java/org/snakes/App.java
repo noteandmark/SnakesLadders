5ee19c87e48c21b054f8f157e62eac7f44781690
Andrei Markov <noteandmark@gmail.com>
2023-08-17 14:17:59 +0300

    old game of 2021 year... and old code without SOLID... just for history

---
+++ src/main/java/org/snakes/App.java
@@ -0,0 +1,10 @@
+package org.snakes;
+
+import org.snakes.gui.MyGUI;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        MyGUI gui = new MyGUI();    }
+}
---
+++ src/main/java/org/snakes/domain/Conus.java
@@ -0,0 +1,30 @@
+package org.snakes.domain;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Conus {
+    private String player;
+    private Point coord;;
+    private BufferedImage image;
+
+    public Conus(String player, Point coord, BufferedImage image) {
+        this.player = player;
+        this.coord = coord;
+        this.image = image;
+    }
+
+    public Conus(){}
+
+    public Point getCoord() {
+        return coord;
+    }
+
+    public void setCoord(Point coord) {
+        this.coord = coord;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+}
